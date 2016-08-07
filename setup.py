49a8e9ab6099f5c156ebf2645793d1d260b12b04
Adam Coldrick <adam@sotk.co.uk>
2016-08-07 23:54:03 +0100

    Add tox.ini
    Add tox to make running tests easy.

---
+++ setup.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2016 Adam Coldrick
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+
+setup(name='yamlui',
+      version='0.0.1',
+      description='Declarative UI for pygame',
+      author='Adam Coldrick',
+      author_email='adam@sotk.co.uk',
+      url='http://www.sotk.co.uk',
+      packages=['yamlui'],
+     )
